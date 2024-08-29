ae7f1b2730982f41f626e4db493a261d96926fb9
swe7z <166190774+swe7z@users.noreply.github.com>
2024-08-29 10:59:59 -0300

    Atualizar o init.lua

--- init.lua
+++ init.lua
@@ -1,25 +1,26 @@
 local GameID = game.PlaceId
 local _ntf = function(message)
-                task.wait()
-                StarterGui:SetCore("SendNotification", {
-                    Title = "Sistema";
-                    Text = message;
-                    Duration = 0.8;
-                    Button1 = "";
-                })
-end;
-
+    task.wait()
+    StarterGui:SetCore("SendNotification", {
+        Title = "Sistema";
+        Text = message;
+        Duration = 0.8;
+        Button1 = "";
+    })
+end
 
 local gameActions = {
-    [10260193230] = pcall(function()
+    [10260193230] = function()
         -- Meme Sea
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/swe7z/ordep-menu/main/release/memesea.lua", true))()
-    end),
+        pcall(function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/swe7z/ordep-menu/main/release/memesea.lua", true))()
+        end)
+    end,
 }
 
 local action = gameActions[GameID]
 if action then
-    pcall(action())
+    pcall(action)  -- Não precisa dos parênteses após `action`
 else
     _ntf("Unsupported Game: " .. GameID)
 end
