e78d8fe3e4eab5d2850b0a145b878a60f5f36fa9
swe7z <166190774+swe7z@users.noreply.github.com>
2024-08-29 11:05:18 -0300

    Atualizar o init.lua

--- init.lua
+++ init.lua
@@ -10,16 +10,13 @@ local _ntf = function(message)
 end
 
 local gameActions = {
-    [10260193230] = function()
-        pcall(function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/swe7z/ordep-menu/main/release/memesea.lua", true))()
-        end)
-    end,
+    [10260193230] = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/swe7z/ordep-menu/main/release/memesea.lua", true))() end,
 }
 
 local action = gameActions[GameID]
 if action then
-    pcall(action)
+		task.wait()
+		action()
 else
     _ntf("Unsupported Game: " .. GameID)
 end
