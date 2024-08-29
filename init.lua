4d18a68dda7137aede225df91e3e30b0de2b5d3b
swe7z <166190774+swe7z@users.noreply.github.com>
2024-08-29 00:13:15 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,25 @@
+local GameID = game.PlaceId
+local init_ntf = function(message)
+                task.wait(0.1)
+                StarterGui:SetCore("SendNotification", {
+                    Title = "Sistema";
+                    Text = message;
+                    Duration = 0.8;
+                    Button1 = "";
+                })
+end;
 
+
+local gameActions = {
+    [10260193230] = pcall(function()
+        -- Meme Sea
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/swe7z/ordep-menu/main/release/memesea.lua", true))()
+    end),
+}
+
+local action = gameActions[GameID]
+if action then
+    action()
+else
+    init_ntf("Unsupported Game: " .. GameID)
+end
